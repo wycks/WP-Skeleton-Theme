67ea85883ca9394b736a1f12a24c7c4a59533fe9
Wycks <bob.ellison@gmail.com>
2011-12-23 14:30:29 -0500

    making 404 page actually use the getskel grid

--- 404.php
+++ 404.php
@@ -4,29 +4,29 @@
  * @subpackage WP-Skeleton
  */
 
-get_header(); ?>
+get_header(); 
+get_template_part( 'menu', 'index' ); //the  menu + logo/site title ?>
 
-
-	<div id="primary">
+	<div class="two-thirds column">
 		<div id="content">
 
-			<article id="post-0" class="post error404 not-found" role="article">
+			<article id="post-0" class="error404">
 				<header class="entry-header">
 					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'WP-Skeleton' ); ?></h1>
 				</header>
 
 				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'WP-Skeleton' ); ?></p>
+				  <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'WP-Skeleton' ); ?></p>
 
-					<?php get_search_form(); ?>
+				     <?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+				     <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
 					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'themename' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 'TRUE', 'title_li' => '', 'number' => '10' ) ); ?>
-						</ul>
+					     <h2 class="widgettitle"><?php _e( 'Most Used Categories', 'themename' ); ?></h2>
+                                             <ul>
+                                             <?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 'TRUE', 'title_li' => '', 'number' => '10' ) ); ?>
+                                             </ul>
 					</div>
 
 					<?php
@@ -34,12 +34,13 @@ get_header(); ?>
 					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 					?>
 
-					
-
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 -->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
+        
 
-<?php get_footer(); ?>
+<?php 
+      get_template_part( 'sidebar', 'index' ); //the Sidebar 
+      get_footer(); ?>
