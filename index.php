b95e397ff1b898b9f35024e1a49e786f8f9f0707
Wycks <bob.ellison@gmail.com>
2011-12-22 23:28:36 -0500

    Moving menu + logo call from header to index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
  */
  
     get_header();  //the Header
-                
+        
+    get_template_part( 'menu', 'index' ); //the  menu + logo/site title 
+    
     get_template_part( 'loop', 'index' ); //the Loop  
      
     get_template_part( 'sidebar', 'index' ); //the Sidebar 
