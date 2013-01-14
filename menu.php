00ba8acb4e3ba6798c44cf1ca704f02f79e53d2a
Wycks <bob.ellison@gmail.com>
2013-01-14 00:27:32 -0500

    phpfog died so changed demo to appfog, also fixed menu

--- menu.php
+++ menu.php
@@ -15,7 +15,7 @@
         </div>
     </div> 
     
-    <div class="eleven columns omega">
+    <div class="offset-by-one omega">
 
     <!--  the Menu -->
     <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
