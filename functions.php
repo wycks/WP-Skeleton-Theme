b7336e474360db1b749f9c8f4c3098133b2918d4
Tom Cahill <me@tomcahill.co.uk>
2014-03-11 10:34:55 +0000

    Make sure base/resets get loaded before all else
    It most likely doesn't make any difference but just making sure that the
    base/reset style sheet gets loaded before any of the others.

--- functions.php
+++ functions.php
@@ -40,8 +40,8 @@ function Wps_load_styles() {
 	wp_register_style( 'skeleton-base', get_template_directory_uri() . '/stylesheets/base.css');
 	wp_register_style( 'skeleton-layout', get_template_directory_uri() . '/stylesheets/layout.css');
 
-	wp_enqueue_style( 'skeleton-style' );
 	wp_enqueue_style( 'skeleton-base' );
+	wp_enqueue_style( 'skeleton-style' );
 	wp_enqueue_style( 'skeleton-layout' );
 
 }
